dac77ae8eb441e9d077cdfa88c25192823f2e604
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-22 09:02:17 +0200

    player class and move method added

--- player.py
+++ player.py
@@ -4,5 +4,22 @@ STARTING_POSITION = (0, -200)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 200
 
-class Player:
-    pass
+
+#2. Create turtle and move with up key
+#3. create method to put turtle at start line after reaching the end
+
+class Player(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.shape("turtle")
+        self.setheading(90)
+
+    def start_pos(self):
+        self.hideturtle()
+        self.goto(STARTING_POSITION)
+        self.showturtle()
+
+    def player_move(self):
+        self.forward(10)
