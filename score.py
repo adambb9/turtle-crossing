2e236e70c5b9a363fc7a0ac18b2062010d86e102
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-22 09:18:35 +0200

    score class added score increases

--- score.py
+++ score.py
@@ -1,6 +1,33 @@
 from turtle import Turtle
 
-FONT = ("Courier", 24, "normal")
+ALIGNMENT = "center"
 
-class Scoreboard:
-    pass
+FONT = ("Courier", 16, "normal")
+
+#4. Create Scoreboard class
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("black")
+        x_pos, y_pos = -230, 250
+        self.goto(x_pos, y_pos)
+        self.score = 1
+        self.update_score()
+
+    #score board writes to screen but doesnt update
+    def update_score(self):
+        self.write(F"Level: {self.score}", align=ALIGNMENT, font=FONT)
+        
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
